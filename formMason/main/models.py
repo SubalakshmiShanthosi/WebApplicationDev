7a0cb884630e37f63bc35157c288109a25bd3480
SubalakshmiShanthosi <=subalakshmicv@gmail.com>
2019-04-26 14:58:02 +0530

    Adding model with one db field form title

--- formMason/main/models.py
+++ formMason/main/models.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
 from django.db import models
-
+from jsonfield import JSONField
 # Create your models here.
+
+
+# Creation of one database field  title
+class FormSchema(models.Model):
+    title=models.CharField(max_length=100)
+    schema=JSONField()
